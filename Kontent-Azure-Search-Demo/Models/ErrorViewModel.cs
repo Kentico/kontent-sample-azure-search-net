a84fc2ee035a70de523cebc947b0709dc11742db
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2020-03-06 09:51:21 -0500

    Add project files.

---
+++ Kontent-Azure-Search-Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kontent_Azure_Search_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
